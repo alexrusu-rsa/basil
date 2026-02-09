0dbb96441aeb3851758b0d7d44f89ab53d167e9f
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 09:22:07 +0200

    feat(welcome): add welcome screen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Welcome } from "./welcome/welcome";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Welcome],
 })
 export class App {
   protected readonly title = signal('basil');

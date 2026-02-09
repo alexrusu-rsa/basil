9ae696e72f6615b217b7827adc7c7dd99c114306
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 22:17:46 +0200

    feat(potting): enhance watering

--- src/app/watering/watering.ts
+++ src/app/watering/watering.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatList, MatListItem } from '@angular/material/list';
+import {
+  MatList,
+  MatListItem,
+  MatListItemIcon,
+  MatListItemLine,
+  MatListItemTitle,
+} from '@angular/material/list';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'b-watering',
-  imports: [RouterLink, MatIconButton, MatIcon, MatList, MatListItem],
+  imports: [
+    RouterLink,
+    MatIconButton,
+    MatIcon,
+    MatList,
+    MatListItem,
+    MatListItemTitle,
+    MatListItemLine,
+    MatListItemIcon,
+  ],
   templateUrl: './watering.html',
 })
 export class Watering {}

f9393731bf109c946569cc76d978f7611586f5aa
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 22:04:58 +0200

    feat(potting): enhance potting

--- src/app/potting/potting.ts
+++ src/app/potting/potting.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatList, MatListItem } from '@angular/material/list';
+import { MatList, MatListItem, MatListItemTitle, MatListItemLine, MatListItemIcon } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'b-potting',
   templateUrl: './potting.html',
-  imports: [RouterLink, MatIconButton, MatIcon, MatList, MatListItem],
+  imports: [RouterLink, MatIconButton, MatIcon, MatList, MatListItem, MatListItemTitle, MatListItemLine, MatListItemIcon],
 })
 export class Potting {}

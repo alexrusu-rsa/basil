c9b151a81edf3f1e9e90e4d4ba2ce102bd766033
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 22:22:45 +0200

    feat(pruning): enhance watering

--- src/app/pruning/pruning.ts
+++ src/app/pruning/pruning.ts
@@ -1,12 +1,27 @@
 import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
-import { MatList, MatListItem } from '@angular/material/list';
+import {
+  MatList,
+  MatListItem,
+  MatListItemIcon,
+  MatListItemLine,
+  MatListItemTitle,
+} from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'b-pruning',
-  imports: [RouterLink, MatList, MatIcon, MatIconButton, MatListItem],
+  imports: [
+    RouterLink,
+    MatIconButton,
+    MatIcon,
+    MatList,
+    MatListItem,
+    MatListItemTitle,
+    MatListItemLine,
+    MatListItemIcon,
+  ],
   templateUrl: './pruning.html',
 })
 export class Pruning {}

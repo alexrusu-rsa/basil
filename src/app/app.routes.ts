ef6d1f4f8354dfe917022ccdb4e790de4b3e9f15
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 10:42:42 +0200

    feat(titles): add titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,18 +11,22 @@ export const routes: Routes = [
   {
     path: 'welcome',
     component: Welcome,
+    title: 'Basil',
   },
   {
     path: 'potting',
     loadComponent: () => import('./potting/potting').then((m) => m.Potting),
+    title: 'Potting',
   },
   {
     path: 'watering',
     loadComponent: () => import('./watering/watering').then((m) => m.Watering),
+    title: 'Watering',
   },
   {
     path: 'pruning',
     loadComponent: () => import('./pruning/pruning').then((m) => m.Pruning),
+    title: 'Pruning',
   },
   {
     path: '**',

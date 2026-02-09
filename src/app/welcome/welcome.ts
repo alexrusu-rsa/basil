36a7ae3733e44672d1b58379f6eab1b0a9d8ac60
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 09:58:59 +0200

    style(global): cleanup styles

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'b-welcome',
   imports: [RouterLink],
   templateUrl: './welcome.html',
-  styleUrl: './welcome.css',
 })
 export class Welcome {}

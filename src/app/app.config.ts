9f3972636ed480c50546b77b80284d0f3633227d
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 10:33:58 +0200

    style(watering, potting): fix icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    { provide: MAT_ICON_DEFAULT_OPTIONS, useValue: { fontSet: 'material-symbols-outlined' } },
+  ],
 };
--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -2,10 +2,11 @@ import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'b-welcome',
-  imports: [RouterLink, UpperCasePipe, MatButton],
+  imports: [RouterLink, UpperCasePipe, MatButton, MatIcon],
   templateUrl: './welcome.html',
 })
 export class Welcome {}

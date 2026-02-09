473f8b07f7b4eebc22b46468c33b543c7502b862
Alex Rusu <rusualexrsa@gmail.com>
2026-02-09 10:24:51 +0200

    style(watering, potting): alignment, navigation

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,9 +1,11 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'b-welcome',
-  imports: [RouterLink],
+  imports: [RouterLink, UpperCasePipe, MatButton],
   templateUrl: './welcome.html',
 })
 export class Welcome {}

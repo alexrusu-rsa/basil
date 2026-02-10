65f96d9b88a68ef517c67aab09d178d2c29d9c02
Alex Rusu <rusualexrsa@gmail.com>
2026-02-10 13:36:00 +0200

    style(global): fix scroll

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  styles: `
+    :host {
+      height: 100vh;
+      div {
+        -webkit-overflow-scrolling: touch;
+      }
+    }
+  `,
   imports: [RouterOutlet],
 })
 export class App {
